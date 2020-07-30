d5280c46e62987deac0115e6a86d08fccb2d7aac
Chris Aka <55612683+boomiin@users.noreply.github.com>
2020-07-30 15:04:40 +0000

    1st form of user profile

--- client/src/components/TheProfile.vue
+++ client/src/components/TheProfile.vue
@@ -2,57 +2,128 @@
   <div class="row justify-content-center">
     <div class="col-11 bg-danger wrapper">
       <div class="row">
-        <div class="col-2">
-          <img
-            style="height: 120px;"
-            src="https://ui-avatars.com/api/?background=0D8ABC&color=fff"
-            class="rounded-circle img-fluid"
-          />
+        <div class="col-4 p-2 bg-white">
+          <div class="radio-buttons">
+            <label class="custom-radio">
+              <input type="radio" name="radio" />
+              <span class="radio-btn">
+                <span class="icon"
+                  ><font-awesome-icon :icon="['fas', 'check']"
+                /></span>
+                <div class="status-icon text-center">
+                  <img class="img-fluid" src="../assets/businessman.svg" />
+                  <h3>Client</h3>
+                </div>
+              </span>
+            </label>
+            <label class="custom-radio">
+              <input type="radio" name="radio" />
+              <span class="radio-btn">
+                <span class="icon"
+                  ><font-awesome-icon :icon="['fas', 'check']"
+                /></span>
+                <div class="status-icon text-center">
+                  <img class="img-fluid" src="../assets/school.svg" />
+                  <h3>Etudiant</h3>
+                </div>
+              </span>
+            </label>
+          </div>
         </div>
-        <div class="col-10">
-          <form @submit.prevent="submitForm" action="/something" method="post">
-            <!-- <div class="error">
-                  Field A is required.
-                </div> -->
+        <div class="col-md-8 p-3">
+          <form
+            @submit.prevent="submitForm"
+            class="form-inline "
+            action="/something"
+            method="post"
+          >
             <div class="form-group">
-              <label for="identif">Identifiant</label>
+              <label for="email">Email</label>
               <input
-                type="text"
+                type="email"
                 class="form-control"
                 autocomplete="off"
-                id="identif"
-                name="identifiant"
-                v-model="identifiant"
+                id="email"
+                name="email"
               />
             </div>
             <div class="form-group">
-              <label for="mdp">Mot de passe</label>
+              <label for="nom">Nom</label>
               <input
                 type="password"
                 class="form-control"
                 autocomplete="off"
-                id="mdp"
-                name="mdp"
-                v-model="mdp"
+                id="nom"
+                name="nom"
               />
             </div>
+            <!-- <div class="form-row">
+              <div class="col">
+                <label for="nom">Nom</label>
+                <input type="text" class="form-control" id="nom" />
+              </div>
+              <div class="col">
+                <label for="nom">Prenom</label>
+                <input type="text" class="form-control" id="prenom" />
+              </div>
+            </div>
+            <div class="form-row">
+              <div class="col-md-6">
+                <label for="nom">Nom</label>
+                <input type="text" class="form-control" id="nom" />
+              </div>
+              <div class="col-md-6">
+                <label for="prenom">Prenom</label>
+                <input type="text" class="form-control" id="prenom" />
+              </div>
+            </div> -->
+          </form>
 
+          <form
+            @submit.prevent="submitForm"
+            class="form-inline "
+            action="/something"
+            method="post"
+          >
             <div class="form-group">
-              <router-link to="/password-forgotten"
-                >Mot de passe oublié ?</router-link
-              >
+              <label for="email">Matricule</label>
+              <input
+                type="email"
+                class="form-control"
+                autocomplete="off"
+                id="email"
+                name="email"
+              />
             </div>
             <div class="form-group">
-              <button type="submit" tag="button" class="button btn-block">
-                Se connecter
-              </button>
+              <label for="nom">Nom</label>
+              <input
+                type="password"
+                class="form-control"
+                autocomplete="off"
+                id="nom"
+                name="nom"
+              />
             </div>
-
             <div class="form-group">
-              <p>
-                Vous n'avez pas de compte ?
-                <router-link to="/inscription">Inscrivez-vous</router-link>
-              </p>
+              <label for="nom">Prenom</label>
+              <input
+                type="password"
+                class="form-control"
+                autocomplete="off"
+                id="nom"
+                name="nom"
+              />
+            </div>
+            <div class="form-group">
+              <label for="nom">Date Naissance</label>
+              <input
+                type="password"
+                class="form-control"
+                autocomplete="off"
+                id="nom"
+                name="nom"
+              />
             </div>
           </form>
         </div>
@@ -62,7 +133,92 @@
 </template>
 
 <script>
-export default { name: "TheProfileVue" };
+export default {
+  name: "TheProfileVue",
+  data() {
+    return {
+      selected: "A",
+      options: [
+        { item: "client", name: "Client" },
+        { item: "etudiant", name: "Etudiant" },
+      ],
+    };
+  },
+  computed: {
+    isClient() {
+      return this.current == 1;
+    },
+    isEtudiant() {
+      return this.current == 2;
+    },
+  },
+};
 </script>
 
-<style lang="css" scoped></style>
+<style lang="css" scoped>
+.radio-buttons {
+  width: 100%;
+  margin: 0 auto;
+  text-align: center;
+}
+
+.custom-radio input {
+  display: none;
+}
+.radio-btn {
+  margin: 10px;
+  width: 180px;
+  height: 200px;
+  border: 3px solid transparent;
+  display: inline-block;
+  border-radius: 10px;
+  position: relative;
+  text-align: center;
+  box-shadow: 0 0 20px #c3c3c367;
+  cursor: pointer;
+}
+
+.radio-btn > .icon {
+  color: #fff;
+  background-color: #348def;
+  font-size: 20px;
+  position: absolute;
+  top: -15px;
+  left: 50%;
+  transform: translateX(-50%) scale(4);
+  border-radius: 50px;
+  padding: 2px 6px 0px 6px;
+  transition: 0.2s;
+  pointer-events: none;
+  opacity: 0;
+}
+
+.radio-btn .status-icon {
+  width: 130px;
+  height: 130px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+/* .radio-btn .status-icon img {
+  height: 60px;
+} */
+
+.radio-btn .status-icon h3 {
+  color: #348def;
+  font-size: 16px;
+  font-weight: 400;
+  text-transform: uppercase;
+}
+
+.custom-radio input:checked + .radio-btn {
+  border: 3px solid #348def;
+}
+
+.custom-radio input:checked + .radio-btn > .icon {
+  border: 3px solid #348def;
+  opacity: 1;
+  transform: translateX(-50%) scale(1);
+}
+</style>
--- client/src/main.js
+++ client/src/main.js
@@ -1,5 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
+import SuiVue from "semantic-ui-vue";
+import "semantic-ui-css/semantic.min.css";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { library } from "@fortawesome/fontawesome-svg-core";
@@ -10,11 +12,29 @@ import {
   faFileAlt,
   faSmileBeam,
   faBars,
+  faCheck,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import router from "./router";
-library.add(faHome, faQuestion, faLightbulb, faFileAlt, faSmileBeam, faBars);
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 
+// Install BootstrapVue
+Vue.use(BootstrapVue);
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin);
+library.add(
+  faHome,
+  faQuestion,
+  faLightbulb,
+  faFileAlt,
+  faSmileBeam,
+  faBars,
+  faCheck
+);
+
+Vue.use(SuiVue);
 Vue.component("font-awesome-icon", FontAwesomeIcon);
 Vue.config.productionTip = false;
 
